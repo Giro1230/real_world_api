6f639cb15f8c2ca61427d43cc3b68966d1cf6de4
Giro <hellowin1230@gmail.com>
2024-11-11 11:32:58 +0900

    초기셋팅

---
+++ src/main/java/com/realworld/RealWorldApiApplication.java
@@ -0,0 +1,13 @@
+package com.realworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealWorldApiApplication.class, args);
+    }
+
+}
